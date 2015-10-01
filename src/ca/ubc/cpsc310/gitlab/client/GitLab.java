6c804de888701838f56f6e5313a4397049f20881
Ren Yi <Crystal@dhcp-206-87-213-105.ubcsecure.wireless.ubc.ca>
2015-10-01 13:25:29 -0700

    string

--- src/ca/ubc/cpsc310/gitlab/client/GitLab.java
+++ src/ca/ubc/cpsc310/gitlab/client/GitLab.java
@@ -47,7 +47,7 @@ public class GitLab implements EntryPoint {
 	 */
 	public void onModuleLoad() 
 	{
-		
+		String s = "boring";
 		service.getUsers(new AsyncCallback<List<IUser>>(){
 
 			@Override
